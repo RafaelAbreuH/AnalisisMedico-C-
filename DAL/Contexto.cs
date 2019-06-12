33389a823c027c5554332ea999ec96002613bb63
Rafael Abreu Hernandez <rafael_abreu5@ucne.edu.do>
2019-06-12 15:56:18 -0400

    Contexto, UsuarioBLL y entidades usuario creadas

---
+++ DAL/Contexto.cs
@@ -0,0 +1,18 @@
+﻿using AnalisisMedico.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnalisisMedico.DAL
+{
+    public class Contexto: DbContext
+    {
+        public DbSet<Usuarios> Usuario { get; set; }
+
+        public Contexto() : base("ConStr")
+        { }
+    }
+}
---
+++ Entidades/Usuarios.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnalisisMedico.Entidades
+{
+    public class Usuarios
+    {
+         
+        [Key]
+        public int UsuarioId { get; set; }
+        public string Nombre { get; set; }
+        public string Telefono { get; set; }
+        public string Sexo { get; set; }
+        public string Sangre { get; set; }
+        public string Email { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+
+        public Usuarios()
+        {
+            UsuarioId = 0;
+            Nombre = String.Empty;
+            Telefono = String.Empty;
+            Sangre = string.Empty;
+            Sexo = string.Empty;
+            Email = String.Empty;
+            FechaNacimiento = DateTime.Now;
+        }
+    }
+}
